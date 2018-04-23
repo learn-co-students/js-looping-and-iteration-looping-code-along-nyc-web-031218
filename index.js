589f796bd47f758544849c837cb1fe969468e924
p-wong <patriciawong08@gmail.com>
2018-04-22 22:57:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges (badge) {
+  for (let i = 0; i < badge.length; i++) {
+    console.log(`Welcome ${badge[i]}! You are employee #${i+1}.`);
+  }
+  return badge;
+}
+
+function maybeTrue () {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails () {
+  let i = 0
+  while (maybeTrue()) {
+    i++;
+  }
+  return(`You got ${i} tails in a row!`);
+}
